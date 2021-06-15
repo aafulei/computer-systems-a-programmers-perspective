007c5b3aa4228030328cf84cff770edf6bb57c20
aafulei <aaron.fu@alumni.ust.hk>
2021-06-15 14:01:39 +0800

    10-04.c 10-05.c

---
+++ 10-system-io/10-04.c
@@ -0,0 +1,3 @@
+/* How would you use dup2 to redirect standard input to descriptor 5? */
+
+// Answer: dup2(5, 0);
---
+++ 10-system-io/10-05.c
@@ -0,0 +1,18 @@
+/* Assuming that the disk file foobar.txt consists of the six ASCII characters
+ * foobar, what is the output of the following program? */
+
+#include "csapp.h"
+
+int main()
+{
+  int fd1, fd2;
+  char c;
+
+  fd1 = Open("foobar.txt", O_RDONLY, 0);
+  fd2 = Open("foobar.txt", O_RDONLY, 0);
+  Read(fd2, &c, 1);
+  Dup2(fd2, fd1);
+  Read(fd1, &c, 1);
+  printf("c = %c\n", c);
+  exit(0);
+}
