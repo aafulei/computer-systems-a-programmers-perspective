5c2456b3652d9c1a25eaff23527b4a9653ba3405
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2021-06-23 22:35:34 +0800

    update

--- 11-network-programming/11-13.c
+++ 11-network-programming/11-13.c
@@ -1,4 +1,4 @@
-/* ---- Homework Problem 11.12 (***) ------------------------------------------
+/* ---- Homework Problem 11.13 (***) ------------------------------------------
  * Modify Tiny so that it deals cleanly (without terminating) with the SIGPIPE
  * signals and EPIPE errors that occur when the write function attempts to
  * write to a prematurely closed connection.
