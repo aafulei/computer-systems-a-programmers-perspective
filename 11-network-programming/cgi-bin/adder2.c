16a8830d4520f033b1f403d16af1d4b72472445f
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2021-06-24 20:18:52 +0800

    adder2

--- 11-network-programming/cgi-bin/adder2.c
+++ 11-network-programming/cgi-bin/adder2.c
@@ -11,13 +11,18 @@ int main(void)
   int n1 = 0, n2 = 0;
 
   /* Extract the two arguments */
-  if ((buf = getenv("QUERY_STRING")) != NULL) {
+  if ((buf = getenv("QUERY_STRING")) != NULL)
+  {
     p = strchr(buf, '&');
     *p = '\0';
     strcpy(arg1, buf);
     strcpy(arg2, p + 1);
-    n1 = atoi(arg1);
-    n2 = atoi(arg2);
+
+    char *equalsign = strchr(arg1, '=');
+    n1 = atoi(equalsign + 1);
+
+    equalsign = strchr(arg2, '=');
+    n2 = atoi(equalsign + 1);
   }
 
   /* Make the response body */
