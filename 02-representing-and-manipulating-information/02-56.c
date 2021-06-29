5666d7406db2d9a156bb75fb3514b70191b5d502
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2021-06-29 08:47:35 +0800

    2-55-57

---
+++ 02-representing-and-manipulating-information/02-56.c
@@ -0,0 +1,157 @@
+/* Homework Problem 2.56  Try running the code for show_bytes for different
+ * sample values. */
+
+// Compiled on a macOS, the byte ordering is little endian.
+// ----------
+// Sample Run
+// ----------
+// $ ./a.out 65535
+// calling test_show_bytes
+//  ff ff 00 00
+//  00 ff 7f 47
+//  ec b8 ce ed fe 7f 00 00
+//
+// $ ./a.out 65536
+// calling test_show_bytes
+//  00 00 01 00
+//  00 00 80 47
+//  ec 58 3d e3 fe 7f 00 00
+//
+// $ ./a.out 2147483647
+// calling test_show_bytes
+//  ff ff ff 7f
+//  00 00 00 4f
+//  ec b8 d6 e3 fe 7f 00 00
+//
+// $ ./a.out -2147483648
+// calling test_show_bytes
+//  00 00 00 80
+//  00 00 00 cf
+//  ec d8 ec e3 fe 7f 00 00
+//
+// $ ./a.out -2147483647
+// calling test_show_bytes
+//  01 00 00 80
+//  00 00 00 cf
+//  ec 88 9e eb fe 7f 00 00
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len)
+{
+  size_t i;
+  for (i = 0; i < len; i++)
+    printf(" %.2x", start[i]);
+  printf("\n");
+}
+
+void show_int(int x)
+{
+  show_bytes((byte_pointer)&x, sizeof(int));
+}
+
+void show_float(float x)
+{
+  show_bytes((byte_pointer)&x, sizeof(float));
+}
+
+void show_pointer(void *x)
+{
+  show_bytes((byte_pointer)&x, sizeof(void *));
+}
+
+void test_show_bytes(int val)
+{
+  int ival = val;
+  float fval = (float)ival;
+  int *pval = &ival;
+  show_int(ival);
+  show_float(fval);
+  show_pointer(pval);
+}
+
+void simple_show_a()
+{
+  int val = 0x87654321;
+  byte_pointer valp = (byte_pointer)&val;
+  show_bytes(valp, 1); /* A. */
+  show_bytes(valp, 2); /* B. */
+  show_bytes(valp, 3); /* C. */
+}
+
+void simple_show_b()
+{
+  int val = 0x12345678;
+  byte_pointer valp = (byte_pointer)&val;
+  show_bytes(valp, 1); /* A. */
+  show_bytes(valp, 2); /* B. */
+  show_bytes(valp, 3); /* C. */
+}
+
+void float_eg()
+{
+  int x = 3490593;
+  float f = (float)x;
+  printf("For x = %d\n", x);
+  show_int(x);
+  show_float(f);
+
+  x = 3510593;
+  f = (float)x;
+  printf("For x = %d\n", x);
+  show_int(x);
+  show_float(f);
+}
+
+void string_ueg()
+{
+  const char *s = "ABCDEF";
+  show_bytes((byte_pointer)s, strlen(s));
+}
+
+void string_leg()
+{
+  const char *s = "abcdef";
+  show_bytes((byte_pointer)s, strlen(s));
+}
+
+void show_twocomp()
+{
+  short x = 12345;
+  short mx = -x;
+
+  show_bytes((byte_pointer)&x, sizeof(short));
+  show_bytes((byte_pointer)&mx, sizeof(short));
+}
+
+int main(int argc, char *argv[])
+{
+  int val = 12345;
+
+  if (argc > 1) {
+    if (argc > 1) {
+      val = strtol(argv[1], NULL, 0);
+    }
+    printf("calling test_show_bytes\n");
+    test_show_bytes(val);
+  }
+  else {
+    printf("calling show_twocomp\n");
+    show_twocomp();
+    printf("Calling simple_show_a\n");
+    simple_show_a();
+    printf("Calling simple_show_b\n");
+    simple_show_b();
+    printf("Calling float_eg\n");
+    float_eg();
+    printf("Calling string_ueg\n");
+    string_ueg();
+    printf("Calling string_leg\n");
+    string_leg();
+  }
+  return 0;
+}
---
+++ 02-representing-and-manipulating-information/02-57.c
@@ -0,0 +1,56 @@
+/* Homework Problem 2.57  Write procedures show_short, show_long, and
+ * show_double that print the byte representations of C objects of types short,
+ * long, and double, respectively. Try these out on several machines. */
+
+#include <stddef.h>
+#include <stdio.h>
+
+void show_short(short x)
+{
+  unsigned char *p = (unsigned char *)&x;
+  for (size_t i = 0, N = sizeof(short); i != N; ++i) {
+    printf("%.2x%c", *p++, (i != N - 1 ? ' ' : '\n'));
+  }
+}
+
+void show_long(long x)
+{
+  unsigned char *p = (unsigned char *)&x;
+  for (size_t i = 0, N = sizeof(long); i != N; ++i) {
+    printf("%.2x%c", *p++, (i != N - 1 ? ' ' : '\n'));
+  }
+}
+
+void show_double(double x)
+{
+  unsigned char *p = (unsigned char *)&x;
+  for (size_t i = 0, N = sizeof(double); i != N; ++i) {
+    printf("%.2x%c", *p++, (i != N - 1 ? ' ' : '\n'));
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  short short_val;
+  long long_val;
+  double double_val;
+  if (argc == 1) {
+    fprintf(stderr, "usage: %s [val=258]\n", argv[0]);
+    short_val = long_val = double_val = 258;
+  }
+  else {
+    if (sscanf(argv[1], "%hd", &short_val) != 1
+        || sscanf(argv[1], "%ld", &long_val) != 1
+        || sscanf(argv[1], "%lf", &double_val) != 1) {
+      fprintf(stderr, "failed to scan value\n");
+      return 1;
+    }
+  }
+  printf("show_short(%hd): ", short_val);
+  show_short(short_val);
+  printf("show_long(%ld): ", long_val);
+  show_long(long_val);
+  printf("show_double(%lf): ", double_val);
+  show_double(double_val);
+  return 0;
+}
