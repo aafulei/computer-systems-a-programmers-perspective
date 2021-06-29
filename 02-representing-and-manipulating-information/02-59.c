6b9610bc1776197663643fd67e0beae4f1701350
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2021-06-29 14:06:51 +0800

    Update 02-59.c

--- 02-representing-and-manipulating-information/02-59.c
+++ 02-representing-and-manipulating-information/02-59.c
@@ -2,6 +2,16 @@
 consisting of the least significant byte of x and the remaining bytes of y.
 For operands x = 0x89ABCDEF and y = 0x76543210, this would give Ox765432EF. */
 
+// ----------
+// Sample Run
+// ----------
+// $ ./a.out
+// usage: ./a.out [x=0x89ABCDEF] [y=0x76543210]
+// mix(2309737967=0x89ABCDEF, 1985229328=0x76543210) = 1985229551 = 0x765432EF
+//
+// $ ./a.out 1234 567890
+// mix(1234=0x4D2, 567890=0x8AA52) = 568018 = 0x8AAD2
+
 #include <stdio.h>
 
 unsigned mix(unsigned x, unsigned y)
@@ -18,7 +28,7 @@ int main(int argc, char *argv[])
   }
   else {
     if (sscanf(argv[1], "%u", &x) != 1 || sscanf(argv[2], "%u", &y) != 1) {
-      fprintf(stderr, "failed to sscanf %s and %s to integers\n", argv[1],
+      fprintf(stderr, "failed to sscanf %s and/or %s to integers\n", argv[1],
               argv[2]);
       return 1;
     }
