92181121a29055084b7a4963bf6e609aa0419ebc
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2021-06-29 20:10:07 +0800

    Update 02-60.c

--- 02-representing-and-manipulating-information/02-60.c
+++ 02-representing-and-manipulating-information/02-60.c
@@ -1,8 +1,42 @@
+// 21/06/29 = Tue
+
 /* Homework Problem 2.60 (**) Suppose we number the bytes in a w-bit word from
 0 (least significant) to w/8 - 1 (most significant). Writy code for the
 following C function, which return an unsigned value in which_byte i of
-argument x has been replaced by byte b: unsigned replace_byte~ (unsigned x,
-'int i, unS'igne'd char b); ji' , -He~e are some examples showing how
-theiunctidn should work: J,, v replace_byte(Ox12345678, 2, OxAB) -->
-Ox12AB?678,
+argument x has been replaced by byte b:
+
+  unsigned replace_byte(unsigned x, int i, unsigned char b);
+
+Here are some examples showing how the function should work:
+
+  replace_byte(0x12345678, 2, 0xAB) --> 0x12AB5678
+  replace_byte(0x12345678, 0, 0xAB) --> 0x123456AB
+
 */
+
+// ----------
+// Sample Run
+// ----------
+// replace_byte(0x12345678, 0, 0xAB) --> 0x123456AB
+// replace_byte(0x12345678, 1, 0xAB) --> 0x1234AB78
+// replace_byte(0x12345678, 2, 0xAB) --> 0x12AB5678
+// replace_byte(0x12345678, 3, 0xAB) --> 0xAB345678
+
+#include <inttypes.h>
+#include <stdio.h>
+
+unsigned replace_byte(unsigned x, int i, unsigned char b)
+{
+  return x & ~(0xff << i * 8) | (b << i * 8);
+}
+
+int main()
+{
+  unsigned x = 0x12345678;
+  int i = 2;
+  unsigned char b = 0xAB;
+  for (int i = 0; i != sizeof x; ++i)
+    printf("replace_byte(0x%X, %d, 0x%X) --> 0x%X\n", x, i, b,
+           replace_byte(x, i, b));
+  return 0;
+}
