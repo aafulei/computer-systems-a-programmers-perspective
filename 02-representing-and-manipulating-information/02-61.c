c01429cb8d38c04628fde8cf00a0fa9092895cbb
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2021-07-05 13:30:34 +0800

    Update 02-61.c

--- 02-representing-and-manipulating-information/02-61.c
+++ 02-representing-and-manipulating-information/02-61.c
@@ -1,3 +1,16 @@
+/* Practice Problem 2.61
+
+  Write C expressions that evaluate to 1 when the following conditions are true
+  and to 0 when they are false. Assume x is of type int.
+
+  A. Any bit of x equals 1.
+
+  B. Any bit of x equals 0.
+
+  C. Any bit in the least significant byte of x equals 1.
+
+  D. Any bit in the most significant byte of x equals 1. */
+
 /* ------------------------------
    Bit-Level Integer Coding Rules
    ------------------------------
@@ -31,19 +44,6 @@
     - INT_MIN and INT_MAX
 
     - casting between int and unsigned */
-
-/* Practice Problem 2.61
-
-  Write C expressions that evaluate to 1 when the following conditions are true
-  and to 0 when they are false. Assume x is of type int.
-
-  A. Any bit of x equals 1.
-
-  B. Any bit of x equals 0.
-
-  C. Any bit in the least significant byte of x equals 1.
-
-  D. Any bit in the most significant byte of x equals 1. */
 
 // ----------
 // Sample Run
